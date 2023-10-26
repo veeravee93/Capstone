c8603a13508985568467352d529e3effc62810d0
Veera <veeravee93@gmail.com>
2023-10-26 07:54:34 +0000

    testfile

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1> CloudDevOps Engineer - Capstone Project </h1>'
+    return '<h1> CloudDevOps Engineer Capstone Project </h1>'
 
 app.run(host='0.0.0.0', port=80)
